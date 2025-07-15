9375edc945e6b0fdc0f550848f9d529807c841a7
Mohamed Mohsen <mohamedm.abdlnabi@gmail.com>
2025-07-15 09:25:12 +0300

    setup development environment and database

--- server.js
+++ server.js
@@ -1,10 +1,30 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+dotenv.config({ path: "./config.env" });
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+if (process.env.NODE_ENV == "development") {
+  app.use(morgan("dev")); // Logging middleware
+  console.log(`Environment: ${process.env.NODE_ENV}`);
+}
+
+async function connectDB(params) {
+  const DATABASE_URL = process.env.DATABASE_URL;
+  await mongoose
+    .connect(DATABASE_URL)
+    .then((result) => console.log("connected to the database"))
+    .catch((err) => console.log("error while connecting to the database"));
+}
+
+connectDB();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log("Server is running on port " + PORT);
 });
