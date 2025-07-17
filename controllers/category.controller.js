fa83639fc8eb5f053b057b6a280a7050b182f8b6
Mohamed Mohsen <mohamedm.abdlnabi@gmail.com>
2025-07-17 08:04:35 +0300

    feat: implement ApiError class for improved error handling

--- controllers/category.controller.js
+++ controllers/category.controller.js
@@ -1,16 +1,13 @@
 import slugify from "slugify";
 import asyncWraper from "../middlewares/asyncWraper.js";
 import Category from "../models/category.model.js";
+import ApiError from "../utils/ApiError.js";
 
 const createCategory = asyncWraper(async (req, res, next) => {
   const category = req.body;
 
   if (!category || !category.name) {
-    return next(
-      res
-        .status(400)
-        .json({ success: false, message: '"Category name is required"' })
-    );
+    return next(new ApiError('"Category name is required"', 400));
   }
 
   const addedCategory = new Category({
@@ -38,11 +35,7 @@ const updateCategory = asyncWraper(async (req, res, next) => {
   const id = req.params.id;
 
   if (!name) {
-    return next(
-      res
-        .status(400)
-        .json({ succes: false, message: "category name is required" })
-    );
+    return next(new ApiError('"Category name is required"', 400));
   }
 
   const updatedCategory = await Category.findByIdAndUpdate(
@@ -55,9 +48,7 @@ const updateCategory = asyncWraper(async (req, res, next) => {
   );
 
   if (!updatedCategory) {
-    return next(
-      res.status(404).json({ success: false, message: "Category not found" })
-    );
+    return next(new ApiError('"Category not found"', 404));
   }
 
   res.status(200).json({ success: true, data: updatedCategory });
@@ -67,13 +58,11 @@ const deleteCategory = asyncWraper(async (req, res, next) => {
   const id = req.params.id;
 
   if (!id) {
-    return next(res.status(400).json({ succes: false, message: "invalid id" }));
+    return next(new ApiError("invalid id", 400));
   }
   const deletedCategory = await Category.findByIdAndDelete(id);
   if (!deletedCategory) {
-    return next(
-      res.status(404).json({ success: false, message: "Category not found" })
-    );
+    return next(new ApiError("Category not found", 404));
   }
   res.status(200).json({ success: true, data: [] });
 });
@@ -82,14 +71,12 @@ const getSpecificCategory = asyncWraper(async (req, res, next) => {
   const id = req.params.id;
 
   if (!id) {
-    return next(res.status(400).json({ succes: false, message: "invalid id" }));
+    return next(new ApiError("invalid id", 400));
   }
   const category = await Category.findById(id);
 
   if (!category) {
-    return next(
-      res.status(404).json({ success: false, message: "category not found" })
-    );
+    return next(new ApiError("Category not found", 404));
   }
 
   res.status(200).json({ success: true, data: category });
